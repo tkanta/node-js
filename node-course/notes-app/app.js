c453cc70ab7a5a77fcfab39a25d65b079ed151fc
tarini <tarininegi@gmail.com>
2019-07-05 04:53:08 +0530

    updates

--- node-course/notes-app/app.js
+++ node-course/notes-app/app.js
@@ -1,7 +1,19 @@
 
+const chalk = require('chalk')
 
-const getNotes = require("./notes")
-console.log(getNotes())
+console.log(chalk.green.bold.inverse('Hi Tarini') + " " + chalk.blue("all good"))
+console.log(chalk.blue.bold('Hello world!'));
+
+console.log(process.argv[2])
+//------------ validator -------------
+// const validator = require('validator')
+
+// console.log(validator.isEmail('tarini@gmail.com'))
+// console.log(validator.isURL('http://tarinigmail.com'))
+
+//--------- importing module ------------
+// const getNotes = require("./notes")
+// console.log(getNotes())
 
 // const add = require("./utils")
 // console.log("sum = "+add(7,4))
