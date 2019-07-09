37752477a0ec8c6f1be06264946bf7e82778833e
tarini <tarininegi@gmail.com>
2019-07-09 07:46:50 +0530

    update

--- node-course/notes-app/app.js
+++ node-course/notes-app/app.js
@@ -1,10 +1,77 @@
 
-const chalk = require('chalk')
+const yargs = require('yargs')
 
-console.log(chalk.green.bold.inverse('Hi Tarini') + " " + chalk.blue("all good"))
-console.log(chalk.blue.bold('Hello world!'));
+const command = process.argv
 
-console.log(process.argv[2])
+//customize yargs command
+yargs.version('1.1.0')
+
+//---------- add, remove, list, read ----------
+
+// create add command
+yargs.command({
+    command:'add',
+    describe: 'Add a new note',
+    builder:{
+        title:{
+            describe:"note title",
+            demandOption: true,
+            type: "string"
+        },
+        body:{
+            describe:"please provide your content for the note",
+            demandOption: true,
+            type: "string"
+        }
+    },
+    handler: function(argv) {
+        console.log('Title : '+ argv.title)
+        console.log('Body : '+ argv.body)
+    }
+})
+
+// create remove command
+yargs.command({
+    command:'remove',
+    describe: 'remove a new note',
+    handler: function() {
+        console.log('remove a new note')
+    }
+})
+
+// create read command
+yargs.command({
+    command:'read',
+    describe: 'read a new note',
+    handler: function() {
+        console.log('read a new note')
+    }
+})
+
+// create list command
+yargs.command({
+    command:'list',
+    describe: 'list a new note',
+    handler: function() {
+        console.log('list a new note')
+    }
+})
+
+yargs.parse()
+
+// if(command === 'add'){
+//     console.log("Adding Note!")
+// }else if(command === 'remove'){
+//     console.log("remove note")
+// }
+
+
+//---------------- Chalk -------------
+// const chalk = require('chalk')
+
+// console.log(chalk.green.bold.inverse('Hi Tarini') + " " + chalk.blue("all good"))
+// console.log(chalk.blue.bold('Hello world!'));
+
 //------------ validator -------------
 // const validator = require('validator')
 
