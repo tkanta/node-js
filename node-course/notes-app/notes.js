c1745d429baad79eecbd9d9ca966ca94df3667ad
tarini <tarininegi@gmail.com>
2019-07-26 07:49:19 +0530

    node save-note code update

--- node-course/notes-app/notes.js
+++ node-course/notes-app/notes.js
@@ -1,10 +1,32 @@
+const fs = require("fs")
 
 const getNotes = function(){
     return "your notes ..."
 }
 
 const addNote = function(title, body){
+    const notes = loadNotes()
+    console.log(notes)
+    notes.push({
+        title: title,
+        body: body
+    })
 
+    saveNotes(notes)
+}
+
+const saveNotes = function(notes) {
+    const dataJson = JSON.stringify(notes)
+}
+
+const loadNotes = function() {
+    try{
+        const dataBuffer = fs.readFileSync('notes.json')
+        const dataJson = dataBuffer.toString()
+        return JSON.parse(dataJson)
+    }catch(e){
+        return []
+    }
 }
 module.exports = {
         getNotes: getNotes,
